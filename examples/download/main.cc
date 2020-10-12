4a7d3ec6afa0c0322aa5044638a012a2e3bbcdc3
Darío <dgrripoll@gmail.com>
2020-10-12 18:32:51 +0200

    Added download klines example

---
+++ examples/download/main.cc
@@ -0,0 +1,108 @@
+#include <algorithm>
+#include <binance.hpp>
+#include <boost/asio/signal_set.hpp>
+#include <boost/program_options.hpp>
+#include <chrono>
+#include <config.hpp>
+#include <iostream>
+
+void parse_args(int argc, char* argv[],
+                boost::program_options::options_description& desc,
+                boost::program_options::variables_map& vm)
+{
+  namespace opt = boost::program_options;
+
+  desc.add_options()("help,h", "Help message")(
+      "config,c", opt::value<std::string>()->default_value("binance.conf"),
+      "Binance configuration file")(
+      "url,U", opt::value<std::string>()->default_value(BINANCE_DEFAULT_URL),
+      "Binance API base URL");
+
+  opt::store(opt::parse_command_line(argc, argv, desc), vm);
+  opt::notify(vm);
+}
+
+class downloader : public std::enable_shared_from_this<downloader>
+{
+  binance::http::stream& api_;
+  binance::http::messages::kline_data kd_;
+
+public:
+  downloader(binance::http::stream& api)
+      : api_(api)
+      , kd_("btcusdt", "4h")
+  {
+    api_.set_rate_limit(1000, boost::posix_time::seconds(60));
+    kd_.set_limit(1500);
+  }
+  ~downloader()
+  {
+    api_.close();
+  }
+  void run()
+  {
+    read();
+  }
+
+private:
+  void read()
+  {
+    int64_t end_time = 0;
+    kd_.get("endTime", end_time);
+    std::cout << "Getting klines with end_time = " << end_time << std::endl;
+    api_.async_read(&kd_, std::bind(&downloader::on_read, shared_from_this(),
+                                    std::placeholders::_1));
+  }
+
+  void on_read(binance::http::messages::kline_data* kd)
+  {
+    int64_t min_time = std::numeric_limits<int64_t>::max();
+    int64_t end_time = 0;
+    for (auto& kl : kd->klines)
+      min_time = std::min(min_time, kl.open_time.time_since_epoch().count());
+
+    kd_.get("endTime", end_time);
+    if (end_time != min_time)
+    {
+      kd_.set_end_time(min_time);
+      read();
+    }
+  }
+};
+
+int main(int argc, char* argv[])
+{
+  boost::program_options::variables_map args;
+  boost::program_options::options_description desc(argv[0]);
+
+  std::cout << "Using Binance version " << BINANCE_FUTURES_VERSION << std::endl;
+
+  parse_args(argc, argv, desc, args);
+  if (args.count("help"))
+  {
+    std::cout << desc << std::endl;
+    return 0;
+  }
+
+  config cnf = config_parser::parse_file(args["config"].as<std::string>());
+
+  binance::io_context ioc;
+  binance::http::stream api(ioc, {cnf.api_key, cnf.api_secret},
+                            args["url"].as<std::string>());
+
+  std::cout << "Connecting to " << args["url"].as<std::string>() << std::endl;
+
+  api.async_connect();
+  std::make_shared<downloader>(api)->run();
+
+  try
+  {
+    ioc.run();
+  }
+  catch (const binance::error& ec)
+  {
+    std::cout << "binance error: " << ec << std::endl;
+  }
+
+  return 0;
+}
